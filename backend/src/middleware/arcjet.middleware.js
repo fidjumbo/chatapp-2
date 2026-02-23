5d081b0b3685c85b190ef8963ae190106484e07d
fidjumbo <fidatemie@gmail.com>
2026-02-23 19:32:25 +0100

    message routes added

--- backend/src/middleware/arcjet.middleware.js
+++ backend/src/middleware/arcjet.middleware.js
@@ -2,6 +2,12 @@ import aj from "../lib/arcjet.js";
 import { isSpoofedBot } from "@arcjet/inspect";
 
 export const arcjetProtection = async (req, res, next) => {
+
+   // 🔥 Skip Arcjet in development
+   if (process.env.NODE_ENV === "development") {
+      return next();
+   }
+
    try {
      const decision = await aj.protect(req);
 
@@ -18,7 +24,6 @@ export const arcjetProtection = async (req, res, next) => {
      }
     }
 
-    // check for spoofed bots
      if (decision.results.some(isSpoofedBot)) {
       return res.status(403).json({
         error: "Spoofed bot detected",
@@ -32,3 +37,39 @@ export const arcjetProtection = async (req, res, next) => {
     next();
    }
 }
+
+
+// import aj from "../lib/arcjet.js";
+// import { isSpoofedBot } from "@arcjet/inspect";
+
+// export const arcjetProtection = async (req, res, next) => {
+//    try {
+//      const decision = await aj.protect(req);
+
+//      if(decision.isDenied()) {
+//         if(decision.reason.isRateLimit()) {
+//           return res.status(429).json({message:"Rate limit exceeded. Please try again later."});
+//         }
+//      else if(decision.reason.isBot()) {
+//        return res.status(403).json({ message: "Bot access denied."});
+//      } else{
+//          return res.status(403).json({
+//           message: "Access denied by security policy.",
+//         });
+//      }
+//     }
+
+//     // check for spoofed bots
+//      if (decision.results.some(isSpoofedBot)) {
+//       return res.status(403).json({
+//         error: "Spoofed bot detected",
+//         message: "Malicious bot activity detected.",
+//       });
+//     }
+
+//     next();
+//    } catch (error) {
+//     console.log("Arcjet Protection Error:", error);
+//     next();
+//    }
+// }
